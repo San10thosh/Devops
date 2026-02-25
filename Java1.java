f970890dd647f7f4f2bca330cdd4956996a2631c
Santhosh S <22cs110@kcgcollege.com>
2026-02-25 11:37:01 +0530

    Add variables and sum calculation in EvenOddChecker

--- Java1.java
+++ Java1.java
@@ -4,6 +4,9 @@ public class EvenOddChecker {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        int a=10;
+        int b=10;
+        int c = a+b;
         System.out.print("Enter a number: ");
         int number = input.nextInt();
 
